98ac66c14f2b653a6e35a320fbb061498a6be9c2
koolaye <knaipo@gmail.com>
2016-01-20 18:26:11 -0800

    notes

--- Week-5b/movie-search.js
+++ Week-5b/movie-search.js
@@ -27,23 +27,37 @@ var array = results["Search"];
      for (var i = 0; i < array.length; i++) {
 
 //adding classes to new elements
-     var li = document.createElement("li");
-     li.classList.add("movie");
+    //  var li = document.createElement("li");
+    //  li.classList.add("movie");
 
-     var title = document.createElement("div");
-     title.classList.add("movie-title");
+     var li = $("<li>").addClass("movie");
+     var title = $("<div>").addClass("movie-title");
+     var date = $("<div>").addClass("movie-release-date");
+     var link = $("<a>").attr("href", "http://www.imdb.com/title/" + array[i]["imdbID"]);
 
-     var date = document.createElement("div");
-     date.classList.add("movie-release-date");
+//anchor: declare variables, add the link,
 
-     var link = document.createElement("a");
-     link.setAttribute("href", "http://www.imdb.com/title/" + array[i]["imdbID"]);
+    //  var title = document.createElement("div");
+    //  title.classList.add("movie-title");
+     //
+    //  var date = document.createElement("div");
+    //  date.classList.add("movie-release-date");
+     //
+    //  var link = document.createElement("a");
+    //  link.setAttribute("href", "http://www.imdb.com/title/" + array[i]["imdbID"]);
 
 //Grabbing results and appending them to a list
-     document.querySelector("#movies").appendChild(li);
-     li.appendChild(title);
-     title.appendChild(link).textContent = results["Search"][i]["Title"];
-     li.appendChild(date).textContent = results["Search"][i]["Year"];
+
+
+    var $li = $("#movies").append($li);
+    var $title = $li.append($title);
+    var $text = $title.append("link").text(array[i]["Title"]);
+    var $year = $li.append("date").text(array[i]["Year"]);
+
+    //  document.querySelector("#movies").appendChild(li);
+    //  li.appendChild(title);
+    //  title.appendChild(link).textContent = array[i]["Title"];
+    //  li.appendChild(date).textContent = array[i]["Year"];
 
   }
 }
