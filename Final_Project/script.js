3a46cf5cd1f18f9c1c502c1f43d31bcbb5e6fc5d
koolaye <knaipo@gmail.com>
2016-01-23 10:16:04 -0800

    project updates

--- Final_Project/script.js
+++ Final_Project/script.js
@@ -1,59 +1,49 @@
-// $('#leftBlock').animate({left: 0});
-//
-// bar script:
-// button you will click on
-// input value is being appended to this
-//
-// bar is clicked
-// page is opened to the form of bar
-// everything else is hidden
-
 // REVEAL INTRO PAGE
 $("#page0").ready(function(displayIntroPage) {
+  $("#leftBlock section h1").addClass("animated slideInRight");
+  $("#rightBlock section h1").addClass("animated slideInLeft");
+  $("#eyes").addClass("animated flash");
   $("#page1").hide();
   $("#page2").hide();
   $("#page3").hide();
 });
 
 //REVEAL FIRST PAGE
-$("#button").on("click", function(displayFirstPage) {
+$("a#button").on("click", function(displayFirstPage) {
   $("#page1").show();
+
   // $('#big1').addClass('animated infinite bounceOutLeft');
 });
 
 
+$("#section").hover(function(){
+    $("a").addClass("animated repeat pulse");
 
-//REVEAL SECOND PAGE, SHOW BAR FORM1, HIDE NETWORKING FORM2
+});
+
+
+//REVEAL SECOND PAGE, SHOW FORMS
+//BAR FORM
 $("#sm1 a").on("click", function(showBar) {
   $("#page2").show();
   $("#bar-form").show();
   $("#networking-form").hide();
   });
 
-
-
-  //REVEAL SECOND PAGE, SHOW NETWORKING FORM2, HIDE BAR FORM1
+//NETWORKING FORM
   $("#med3 a").on("click", function(showNetworking) {
     $("#page2").show();
     $("#bar-form").hide();
     $("#networking-form").show();
   });
 
-    // $("div.circle, a #networking-form").on("click", function(showNetworking) {
-    // $("#networking-form").show();
-    // $("#bar-form").hide();
-    // });
 
 
 //REVEAL THIRD PAGE WHEN BUTTON IS CLICKED, HIDE SCRIPT2
-// $("div#bar-form, #button2").on("click", function(createScript) {
-
   $("#bar-form").find("#button2").on("click", function(createScript) {
-
   $("#page3").show();
   $("#bar-script").show();
   $("#networking-script").hide();
-
 
   $(".line1").append($(".greeting").val());
   $(".line2").append($(".sport-name").val());
@@ -78,4 +68,16 @@ $("#networking-form").find("#button2").on("click", function(createScript) {
   $(".line5").append($(".time-length2").val());
   $(".line6").append($(".subject").val());
   $(".line7").append($(".removed").val());
+});
+
+
+
+jQuery(function ($) {
+    $("#query").tweets;
+        avatar_size: 16; // avatar size in px
+        count: 5; // how many tweets to show
+        query: "#microsoft"; // search query
+        loading_text: "searching twitter...";
+        refresh_interval: 60; // seconds before next refresh
+
 });
